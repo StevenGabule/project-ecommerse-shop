b78e2ded9a269f0baa7d56120cbd2b4ab2f08ea4
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-03-10 09:33:45 +0800

    working on the page template on the front page and install dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to ProShops</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
