09fcf9342fffcaebe9a6da54c8adbefa83502dcd
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-03-20 00:59:21 +0800

    adding payment screen and save payment method

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreens from "./screens/LoginScreens";
+import RegisterScreens from "./screens/RegisterScreens";
+import ProfileScreens from "./screens/ProfileScreens";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreens from "./screens/PaymentScreens";
 
 function App() {
   return (
@@ -16,6 +21,11 @@ function App() {
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreens} />
+          <Route path="/register" component={RegisterScreens} />
+          <Route path="/profile" component={ProfileScreens} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreens} />
         </Container>
       </main>
       <Footer />
---
+++ src/reducers/userReducers.js
@@ -0,0 +1,55 @@
+import { USER } from "../constants/userConstants";
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER.USER_LOGIN_REQUEST:
+      return { loading: true };
+    case USER.USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER.USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case USER.USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER.REGISTER_REQUEST:
+      return { loading: true };
+    case USER.REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER.REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER.DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case USER.DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER.DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER.UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER.UPDATE_PROFILE_SUCCESS:
+      return { loading: false, userInfo: action.payload, success: true };
+    case USER.UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
